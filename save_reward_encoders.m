93a42a83f77636d3ec73ffd1f26a070e80552252
Daniel Bliss <dbliss@berkeley.edu>
2012-03-29 20:23:23 -0700

    Update save_reward_encoders.

--- save_reward_encoders.m
+++ save_reward_encoders.m
@@ -1,24 +1,30 @@
-cd ~/data/wallis/
-session_dirs = dir;
+cd ~/Science/wallis/regression_results
+mat_files = dir;
 pos_encoders = [];
 neg_encoders = [];
-for i = 1:length(session_dirs)
-    session = session_dirs(i).name;
-    if session(1) == 'A' | session(1) == 'B'
-        cd(session)
-        [SpikeInfo, ~, ~, ~] = spk_read([session '.spk']);
-        for j = 1:length(SpikeInfo.NeuronID)
-            neuron = int2str(SpikeInfo.NeuronID(j));
-            load([neuron '_reward_regression'], 'stats');
-            if stats.fstat.pval < 0.001
-                if stats.beta(2) < 0
-                    neg_encoders = [neg_encoders; [session '-' neuron]];
-                elseif stats.beta(2) > 0
-                    pos_encoders = [pos_encoders; [session '-' neuron]];
+mixed_encoders = [];
+for i = 3:length(mat_files)
+    neuron = mat_files(i).name;
+    if regexp(neuron, '[AB][0-9]{3}-[0-9]{4}.mat')
+        load(neuron(1:end-4), 'betas', 'r_squareds', 'p_values');
+        pos_count = 0;
+        neg_count = 0;
+        for j = 32:200
+            if p_values(j) < 0.001
+                if betas(j) < 0
+                    neg_count = neg_count + 1;
+                else
+                    pos_count = pos_count + 1;
                 end
             end
         end
-        cd('..')
+        if pos_count > 0 & neg_count > 0
+            mixed_encoders = [mixed_encoders; neuron(1:end-4)];
+        elseif pos_count > 0
+            pos_encoders = [pos_encoders; neuron(1:end-4)];
+        elseif neg_count > 0
+            neg_encoders = [neg_encoders; neuron(1:end-4)];
+        end
     end
 end
-save('encoders', 'pos_encoders', 'neg_encoders')
+save('encoders', 'pos_encoders', 'neg_encoders', 'mixed_encoders')
