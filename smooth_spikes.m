98eecc0eaad9eed9f8da0742ac93cf4723751106
Daniel Bliss <dbliss@berkeley.edu>
2012-04-02 16:09:19 -0700

    Fix error in smooth_spikes.

--- smooth_spikes.m
+++ smooth_spikes.m
@@ -2,5 +2,5 @@ function smoothed_spikes = smooth_spikes(trial_spikes)
 % smoothed_spikes = smooth_spikes(trial_spikes)
 
 for i = 1:size(trial_spikes, 2)-99
-    smoothed_spikes(:, i) = mean(condition_spikes(:, i:i+99), 2);
+    smoothed_spikes(:, i) = mean(trial_spikes(:, i:i+99), 2);
 end
