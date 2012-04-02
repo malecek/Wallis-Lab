23704d550257d5fa46036569a19983c1c0f4e664
Daniel Bliss <dbliss@berkeley.edu>
2012-04-02 16:10:09 -0700

    Update plot_mean_firing_rate.

--- plot_mean_firing_rate.m
+++ plot_mean_firing_rate.m
@@ -1,36 +1,24 @@
-function plot_mean_firing_rate(varargin)
-% 1) plot_mean_firing_rate(session, neuron, SpikeInfo, SpikeData)
-% 2) plot_mean_firing_rate(neuron_matrix, encoding_sign)
-% 
-% neuron_matrix is neurons X conditions X ms relative to reward cue
-% (-500 to 3250).
+function plot_mean_firing_rate(session, neuron, SpikeInfo, SpikeData)
+% plot_mean_firing_rate(session, neuron, SpikeInfo, SpikeData)
 
-if length(varargin) == 4
-    session = varargin{1};
-    neuron = varargin{2};
-    SpikeInfo = varargin{3};
-    SpikeData = varargin{4};
-    mean_firing_rate = get_mean_firing_rate(neuron, SpikeInfo, SpikeData);
-else
-    neuron_matrix = varargin{1};
-    encoding_sign = varargin{2};
-    mean_firing_rate = mean(neuron_matrix, 1);
-    std_error = squeeze(std(mean_firing_rate)) / sqrt(size(neuron_matrix,1));
-end
-colors = [0 0 1; 0.25 0 0.75; 0.5 0 0.5; 0.75 0 0.25; 1 0 0];
+trial_spikes = get_trial_spikes(SpikeInfo, SpikeData, neuron);
+trial_spikes = smooth_spikes(trial_spikes);
+reward_conditions = get_reward_conditions(SpikeInfo);
+mean_firing_rate = get_condition_spikes(reward_conditions, trial_spikes);
+colors = [0 0 0; 0 0 1; 0.5 0.3 0.8; 1 0.3 0.6; 1 0 0];
+neuron = int2str(neuron);
 figure;
 hold on
 for i=1:5
-    [hl,hp]=boundedline(1:size(mean_firing_rate,2),mean_firing_rate(i,:),...
-                        std_error(i,:),colors(i,:));
-    set(get(get(hp,'Annotation'),'LegendInformation'),'IconDisplayStyle','off')
+    plot(mean_firing_rate(i, :), 'Color', colors(i, :))
 end
+y_range = get(gca, 'ylim');
+plot([501 501], y_range, 'k', 'LineWidth', 1.5)
+plot([2001 2001], y_range, 'k', 'LineWidth', 1.5)
+title([session '-' neuron])
 ylabel('Firing Rate (Hz)');
 set(gca, 'XTick', 501:500:3501, 'XTickLabel', 0:500:3000);
 xlabel('Time Relative to Reward Cue (ms)')
-legend('0.1 g', '0.2 g', '0.3 g', '0.4 g', '0.5 g', 'Location', 'SouthEast');
-if length(varargin) == 4
-    print(gcf, ['mean_firing_rate_' session '_' neuron], '-dpdf');
-else
-    print(gcf, ['grand_average_firing_rate_' encoding_sign], '-dpdf');
-fclose('all');
+legend('0.1 g', '0.2 g', '0.3 g', '0.4 g', '0.5 g', 'Location', 'Best');
+print(['~/Science/wallis/figures/mean_firing_rate_' session '_' neuron],...
+      '-djpeg');
