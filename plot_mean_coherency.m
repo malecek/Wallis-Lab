7057cfa384b75b6ba30b5f2f60eabc7b29376c6b
Daniel Bliss <dbliss@berkeley.edu>
2012-04-03 15:48:45 -0700

    Remove coherency data for frequencies > 300 Hz.

--- plot_mean_coherency.m
+++ plot_mean_coherency.m
@@ -1,8 +1,13 @@
 cd ~/Science/wallis/coherencies
+% Remove data for frequencies > 300 Hz.
 load pos_coherencies 
+pos_coherencies = pos_coherencies(:, 1:615);
 load mixed_coherencies 
+mixed_coherencies = mixed_coherencies(:, 1:615);
 load neg_coherencies
+neg_coherencies = neg_coherencies(:, 1:615);
 load('frequencies', 'fs')
+fs = fs(1:615);
 mean_pos_c = nanmean(pos_coherencies, 1);
 se_pos_c = nanstd(pos_coherencies) / sqrt(size(pos_coherencies, 1));
 mean_neg_c = nanmean(neg_coherencies, 1);
