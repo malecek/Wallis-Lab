32bb3fdc767ea75c8bb4645466d278862373cc5a
Daniel Bliss <dbliss@berkeley.edu>
2012-04-02 23:41:35 -0700

    Update plot_LFP_encoders and README; add figure.

--- plot_LFP_encoders.m
+++ plot_LFP_encoders.m
@@ -1,36 +1,34 @@
-cd ~/data/wallis/
+cd ~/Science/wallis/LFP_regression
+load('encoders', 'pos_encoders', 'neg_encoders')
 frequencies = 2.^(0:0.5:8);
-load LFP_encoders
-freq_counts = zeros(1, 17);
-for i = 1:size(pos_encoders, 1)
-    encoder = pos_encoders(i, :);
-    [subject, elec_freq] = strtok(encoder, '-');
-    elec_freq = elec_freq(2:end);
-    [elec, freq] = strtok(elec_freq, '-');
-    freq = str2num(freq(2:end));
-    freq_counts(freq) = freq_counts(freq) + 1;
+% The frequency values saved in encoders are only to four decimal
+% places (not rounded, but the first four in the actual numbers).
+% In order to compare them to the actual frequencies, we need to
+% round to three decimal places.
+for i = 1:length(frequencies)
+    frequencies(i) = round2(frequencies(i), 1e-3);
 end
+encoder_types = ['pos'; 'neg'];
+for i = 1:size(encoder_types, 1)
+    sign = encoder_types(i, :);
+    eval([sign '_counts = zeros(1, 17);'])
+    for j = 1:size(eval([sign '_encoders']), 2)
+        match = regexp(eval([sign '_encoders{j}']), '-', 'split');
+        freq = str2num(regexprep(match{3}, '_', '.'));
+        freq = round2(freq, 1e-3);
+        i_freq = find(frequencies == freq);
+        eval([sign '_counts(i_freq) = ' sign '_counts(i_freq) + 1;'])
+    end
+end
+colors = 'rb';
 figure
-subplot(2, 1, 1);
 hold on
-bar(freq_counts)
-xlabel('Frequencies (Hz)')
-ylabel('Number of Positively Encoding Electrodes')
-set(gca, 'XTick', 1:2:17, 'XTickLabel', frequencies(1:2:end))
-
-freq_counts = zeros(1, 17);
-for i = 1:size(neg_encoders, 1)
-    encoder = neg_encoders(i, :);
-    [subject, elec_freq] = strtok(encoder, '-');
-    elec_freq = elec_freq(2:end);
-    [elec, freq] = strtok(elec_freq, '-');
-    freq = str2num(freq(2:end));
-    freq_counts(freq) = freq_counts(freq) + 1;
+for i = 1:size(encoder_types, 1)
+    sign = encoder_types(i, :);
+    plot(eval([sign '_counts']), colors(i))
 end
-subplot(2, 1, 2);
-hold on
-bar(freq_counts)
 xlabel('Frequencies (Hz)')
-ylabel('Number of Positively Encoding Electrodes')
+ylabel('Number of Encoding Electrodes')
+legend('Positive', 'Negative', 'Location', 'Best')
 set(gca, 'XTick', 1:2:17, 'XTickLabel', frequencies(1:2:end))
-print('-dpdf', 'frequency_encoding_histogram')
+print('-dpdf', '~/Science/wallis/figures/LFP_encoders')
